ebdfcb31ba4c45592fee33872bdec4e5410d6fb4
cj lindblad <carljohan.lindblad@gmail.com>
2020-06-12 12:44:25 +0200

    Added google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,6 +6,12 @@ module.exports = {
     author: `@gatsbyjs`,
   },
   plugins: [
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: "UA-169254580-1",
+      },
+    },
     `gatsby-plugin-postcss`,
     `gatsby-plugin-react-helmet`,
     {
